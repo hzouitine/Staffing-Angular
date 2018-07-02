b25f7cf38d51813ffffb6e3f4fa499ad722b5364
soufiane-najim <soufianenajime@gmail.com>
2018-07-02 14:13:22 +0100

    init

---
+++ src/app/header/header-filter/header-filter.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderFilterComponent } from './header-filter.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderFilterComponent]
+})
+export class HeaderFilterModule { }
---
+++ src/app/header/header-search/header-search.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderSearchComponent } from './header-search.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderSearchComponent]
+})
+export class HeaderSearchModule { }
