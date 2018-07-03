605a957d5049f5df0a6c3c0579b17910b279e1fc
hzouitine <3zouitine@gmail.com>
2018-07-03 11:48:33 +0100

    Add component pagination

--- src/app/header/header-search/pagination/pagination.component.ts
+++ src/app/header/header-search/pagination/pagination.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterContentInit, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit{
 
-  constructor() { }
+  private pageSelected : number = 1;
+  private numResources : number;
+  private resByPageSelectedIndex : number = 2;
+  private resByPage : number[] = [5,10,15,20,25,30];
+  
+  constructor() {
+    this.numResources = 51;
 
+  }
   ngOnInit() {
+    console.log('pages() in oninit',this.pages())
+  }
+
+
+  numberOfPages() {
+    if( this.numResources%this.resByPage[this.resByPageSelectedIndex ] == 0)
+      return parseInt(this.numResources/this.resByPage[this.resByPageSelectedIndex ]+"")
+    else
+      return parseInt(this.numResources/this.resByPage[this.resByPageSelectedIndex ]+"") + 1;
+    
+  }
+
+  pages(){
+    let _pages : number[] = [];
+    for(var i=1;i<=this.numberOfPages();i++){
+      _pages.push(i);
+    }
+    return _pages;
+  }
+
+  navigate(v){
+    if(this.pageSelected + v <= this.numberOfPages() && this.pageSelected + v > 0)
+      this.pageSelected = this.pageSelected + v;
+  }
+  onselectionChange(){
+    console.log("Send Request");
+
   }
 
 }
