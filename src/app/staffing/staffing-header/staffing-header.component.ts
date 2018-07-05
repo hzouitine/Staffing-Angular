f9ddec808e0da2ef571bf8f9e3949bfaff79caad
soufiane-najim <soufianenajime@gmail.com>
2018-07-05 15:44:21 +0100

    staffing header validated

--- src/app/staffing/staffing-header/staffing-header.component.ts
+++ src/app/staffing/staffing-header/staffing-header.component.ts
@@ -6,7 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staffing-header.component.scss']
 })
 export class StaffingHeaderComponent implements OnInit {
-  daynumber  = ["1","2","3","4","5"];
+  months = [{
+    month: "July 2018",
+    numberOfWeeks: 4
+  },
+  {
+    month: "August 2018",
+    numberOfWeeks: 4
+  }];
+  weeks = [1, 2, 3, 4, 5, 6, 7, 8]
+  days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,25,26]
+  data = {
+    "months": [{
+      "month": "July 2018",
+      "weeks": [
+        {
+          week: 66,
+          days: [1, 2, 3, 4, 5, 6, 7]
+        },
+        {
+          week: 23,
+          days: [11, 12, 13, 41, 15, 16, 71]
+        },
+        {
+          week: 62,
+          days: [1, 2, 3, 4, 5, 6, 7]
+        },
+        {
+          week: 63,
+          days: [11, 12, 13, 41, 15, 16, 71]
+        }
+      ]
+    },
+    {
+      "month": "August 2018",
+      "weeks": [
+        {
+          week: 77,
+          days: [1, 2, 3, 4, 5, 6, 7]
+        },
+        {
+          week: 87,
+          days: [11, 12, 13, 41, 15, 16, 71]
+        }
+      ]
+    }]
+  };
+
+
+
   constructor() { }
 
   ngOnInit() {
