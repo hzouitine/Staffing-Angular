7ec2141c31095c478329fdf3955843d64345ef87
soufiane-najim <soufianenajime@gmail.com>
2018-07-06 13:36:55 +0100

    add drag n drop

--- src/app/staffing/staffing-block/row-staffing/row-staffing.component.ts
+++ src/app/staffing/staffing-block/row-staffing/row-staffing.component.ts
@@ -9,6 +9,7 @@ export class RowStaffingComponent implements OnInit {
 
   dureeStaffing = [];
 
+  draggedTd : any = null;
   constructor() { }
 
   ngOnInit() {
@@ -17,6 +18,27 @@ export class RowStaffingComponent implements OnInit {
     for (let i = 0; i < 56; i++) {
       this.dureeStaffing.push(d[Math.floor(d.length * Math.random())]);
     }
+  }
+
+  onDragstart(e){
+    console.log();
+    this.draggedTd = e.target;
+    console.log(e.target);
+  }
+
+  onDragover(e) {
+    console.log(e.target);
+    if(e.target.tagName.toUpperCase() === "TD" && !e.target.querySelector("button") && this.draggedTd){
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  }
+
+  onDrop(e){
+    e.preventDefault();
+    e.stopPropagation();
+    e.target.append(this.draggedTd.querySelector("button"));
+    this.draggedTd = null;
   }
 
 }
