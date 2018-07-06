cb678f9d5eb174972483f36cfeed420a77b25139
soufiane-najim <soufianenajime@gmail.com>
2018-07-06 12:35:19 +0100

    Add row staffing component

--- src/app/staffing/staffing-block/row-staffing/row-staffing.component.ts
+++ src/app/staffing/staffing-block/row-staffing/row-staffing.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-row-staffing',
+  selector: '[app-row-staffing]',
   templateUrl: './row-staffing.component.html',
   styleUrls: ['./row-staffing.component.scss']
 })
 export class RowStaffingComponent implements OnInit {
 
+  dureeStaffing = [];
+
   constructor() { }
 
   ngOnInit() {
+
+    let d = ["1d", "1/2d",""];
+    for (let i = 0; i < 56; i++) {
+      this.dureeStaffing.push(d[Math.floor(d.length * Math.random())]);
+    }
   }
 
 }
