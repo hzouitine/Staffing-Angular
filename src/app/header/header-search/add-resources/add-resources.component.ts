35e5dc0a44429eaaac08da00f9bd703a734e737a
soufiane-najim <soufianenajime@gmail.com>
2018-07-20 11:39:07 +0100

    add image

--- src/app/header/header-search/add-resources/add-resources.component.ts
+++ src/app/header/header-search/add-resources/add-resources.component.ts
@@ -8,13 +8,18 @@ import { ResourcesListComponent } from './resources-list/resources-list.componen
   styleUrls: ['./add-resources.component.scss']
 })
 export class AddResourcesComponent implements OnInit {
-  length = [];
+  length = ['https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL5eKx_XhVZbi5LScv9nJ1uIZ8nShsCDKNUOje9j6d-XkNXYwiHA',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoeJRHEgvxBWiUky6Us0FMQyk9W7BQzy8XjnQl2npL6ZHCO9w',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9UZao3vkPeKiuVego9dDzWzF2rEh8qOKKfrk8ddLgNA9wXcIq',
+    'https://pmctvline2.files.wordpress.com/2016/05/person-of-interest-root-dies-amy-acker.jpg?w=620&h=415&crop=1'
+  ];
   constructor(public dialog: MatDialog) { }
 
   createModel() {
     const dialogRef = this.dialog.open(ResourcesListComponent);
     dialogRef.afterClosed().subscribe(result => {
-      this.length.push('https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350');
+
     });
   }
 
